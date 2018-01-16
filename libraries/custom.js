4ebdb4c56a2f0e7ce41a8fce4323c42eb9353288
Rinesh Bharath <rinesh93bharath@hotmail.com>
2018-01-16 17:41:02 +0530

    Add files via upload

---
+++ libraries/custom.js
@@ -0,0 +1,7 @@
+/*custom javascript document*/
+$( document ).ready(function() {
+    $('#ifr').click(function(){
+  		$("#loading").attr("src","Magazine Format Type/3 MAG with DNR.html");
+	});
+});
+
